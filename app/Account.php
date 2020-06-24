5c771de486a4b0e616b00c21896b8a5f38416d02
arked <arek@mys.pl>
2020-06-24 17:59:23 +0200

    Hello świat

--- app/Account.php
+++ app/Account.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    public function regions()
+    {
+        return $this->belongsTo('App\Region');
+    }
+
+    public function region()
+    {
+        return $this->hasOne('App\Region');
+    }
 }
--- app/Region.php
+++ app/Region.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
-    //
+    public function accounts()
+    {
+        return $this->belongsTo('App\Account');
+    }
+
+    public function account()
+    {
+        return $this->hasMany('App\Account');
+    }
 }
