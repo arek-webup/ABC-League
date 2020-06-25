af0ad5c1422a73f931e30117e1141f639ea1e100
arked <arek@mys.pl>
2020-06-25 19:57:01 +0200

    Hello świat

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
 
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: *');
-        header('Access-Control-Allow-Headers: *');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
         // ALLOW OPTIONS METHOD
         $headers = [
             'Access-Control-Allow-Methods'=> 'GET, POST, OPTIONS, PUT, DELETE',
