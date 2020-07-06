cc7600a0b277761ef4e73b7412655e7ac3ef1294
arked <arek@mys.pl>
2020-07-06 19:32:35 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::group([
     Route::get('/availableregions', 'ApiController@available_regions');
 
     Route::get('/reviews', 'ApiController@reviews');
-    Route::post('/reviews/add', 'ApiController@add_review');
+    Route::get('/reviews/add', 'ApiController@add_review');
 
     Route::get('/convert/{price}/{curr}/{curr_sec}', 'ApiController@covert');
     Route::get('/currency', 'ApiController@getCurrency');
