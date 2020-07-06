a7e3129418c12975747acdfad26aa07d7473c86a
arked <arek@mys.pl>
2020-07-06 19:28:43 +0200

    siema

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('cors')->group(function () {
+Route::group([
+
+    'middleware' => ['api'],
+    'prefix' => 'auth'
+
+], function () {
+
     Route::get('/', 'HomeController@index');
     Route::get('/accounts', 'ApiController@accounts');
     Route::get('/accounts/{id}', 'ApiController@acc');
@@ -41,6 +47,30 @@ Route::middleware('cors')->group(function () {
     Route::get('paymentsuccess', 'PaymentController@payment_success');
     Route::get('paymenterror', 'PaymentController@payment_error');
 });
+
+//Route::middleware('cors')->group(function () {
+//    Route::get('/', 'HomeController@index');
+//    Route::get('/accounts', 'ApiController@accounts');
+//    Route::get('/accounts/{id}', 'ApiController@acc');
+//    Route::get('/accounts/region/{id}', 'ApiController@accfromregion');
+//
+//    Route::get('/available/accounts/{id}', 'ApiController@getAccountsCount');
+////    Route::get('/available/accounts/region/{id}', 'ApiController@available_accfromregion');
+//
+//    Route::get('/regions', 'ApiController@regions');
+//    Route::get('/region/{id}', 'ApiController@getregion');
+//    Route::get('/availableregions', 'ApiController@available_regions');
+//
+//    Route::get('/reviews', 'ApiController@reviews');
+//    Route::post('/reviews/add', 'ApiController@add_review');
+//
+//    Route::get('/convert/{price}/{curr}/{curr_sec}', 'ApiController@covert');
+//    Route::get('/currency', 'ApiController@getCurrency');
+//    Route::get('/pay_stripe', 'PaymentController@pay_stripe');
+//    Route::post('/pay_paypal', 'PaymentController@pay_paypal')->name('charge');
+//    Route::get('paymentsuccess', 'PaymentController@payment_success');
+//    Route::get('paymenterror', 'PaymentController@payment_error');
+//});
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware('cors')->group(function () {
+//Route::middleware('cors')->group(function () {
+//    Route::get('/', 'HomeController@index');
+//    Route::get('/accounts', 'ApiController@accounts');
+//    Route::get('/accounts/{id}', 'ApiController@acc');
+//    Route::get('/accounts/region/{id}', 'ApiController@accfromregion');
+//
+//    Route::get('/available/accounts/{id}', 'ApiController@getAccountsCount');
+////    Route::get('/available/accounts/region/{id}', 'ApiController@available_accfromregion');
+//
+//    Route::get('/regions', 'ApiController@regions');
+//    Route::get('/region/{id}', 'ApiController@getregion');
+//    Route::get('/availableregions', 'ApiController@available_regions');
+//
+//    Route::get('/reviews', 'ApiController@reviews');
+//    Route::post('/reviews/add', 'ApiController@add_review');
+//
+//    Route::get('/convert/{price}/{curr}/{curr_sec}', 'ApiController@covert');
+//    Route::get('/currency', 'ApiController@getCurrency');
+//    Route::get('/pay_stripe', 'PaymentController@pay_stripe');
+//    Route::post('/pay_paypal', 'PaymentController@pay_paypal')->name('charge');
+//    Route::get('paymentsuccess', 'PaymentController@payment_success');
+//    Route::get('paymenterror', 'PaymentController@payment_error');
+//});
+
+Route::group([
+
+    'middleware' => ['api'],
+    'prefix' => 'auth'
+
+], function () {
+
     Route::get('/', 'HomeController@index');
     Route::get('/accounts', 'ApiController@accounts');
     Route::get('/accounts/{id}', 'ApiController@acc');
@@ -35,8 +65,6 @@ Route::middleware('cors')->group(function () {
     Route::get('paymentsuccess', 'PaymentController@payment_success');
     Route::get('paymenterror', 'PaymentController@payment_error');
 });
-
-
 
 Auth::routes();
 
